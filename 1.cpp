def9d789544cc15d5684fcd3ed778dec57b6a6fe
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2022-05-23 01:03:43 +0300

    part 2

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
     char s[40], temp1[40];
-    int w, h, i, j, count;
-    double temp2, result;
+    int w, h, i, j, temp2;
+    double temp, result;
     // открываем файл для чтения
     FILE *f1 = fopen("matr.txt", "r");
     // проверка на ошибки открытия
@@ -18,36 +17,23 @@ int main() {
             puts(s);
         // на начало файла
         rewind(f1);
-        // узнаем размер матрицы, он не больше 9х9 (если больше, надо дописать программу с поиском пробела), читаем 1 строку
-        fgets(s, 40, f1);
 
         // в 1 строке надо найти длину и высоту матрицы в виде двух целых чисел
-        i = 0;
-        // пока не конец строки
-        while (s[i] != '\n') {
-            // найти пробел
-            if (s[i] == ' ') {
-                // находим длина
-                strncpy(temp1, s, i);
-                w = atof(temp1);
-                // находим высота (+2 т.к. число после пробела)
-                strncpy(temp1, s + 2, i);
-                h = atof(temp1);
-            }
-            i++;
-        }
+        fscanf(f1, "%d", &temp2);
+        w = temp2;
+        fscanf(f1, "%d", &temp2);
+        h = temp2;
 
-        //
-        for(i = 0; i < h; i++)
-        {
-            for (j = 0 ; j < w; j++)
-            {
-                fscanf(f1,"%lf",&temp2);
-                if (temp2 < 0)
-                    result = result + temp2;
-                printf("%.2f ",temp2);
+        // столбцы матрицы
+        for (i = 0; i < h; i++) {
+            // строки
+            for (j = 0; j < w; j++) {
+                // находим double
+                fscanf(f1, "%lf", &temp);
+                // если отрицательное, находим сумму этих элементов
+                if (temp < 0)
+                    result = result + temp;
             }
-            printf("\n");
         }
 
         printf("Нашли сумму отрицательных элементов матрицы. Она равна %.2f", result);
