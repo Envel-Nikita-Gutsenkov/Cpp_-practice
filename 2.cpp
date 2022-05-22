2727e42f37a98aa0c7e55ff8bd4a477a365e71b6
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2022-05-23 01:58:29 +0300

    part 4

---
+++ 2.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int count, i, positive = 0;
+    float temp, percent;
+    char s[40];
+    // открываем файл для чтения и записи
+    FILE *f1 = fopen("chisla.txt", "r+");
+
+    if (!f1) {
+        printf("ОШИБКА ЧТЕНИЯ");
+    } else {
+        printf("Все содержимое файла:\n");
+        // выводим все
+        while (fgets(s, 40, f1))
+            puts(s);
+        // возврат в начало файла
+        rewind(f1);
+        // получаем число в первой строке
+        fscanf(f1, "%d", &count);
+        // получаем все числа во второй строке
+        for (i = 0; i < count; i++) {
+            fscanf(f1, "%f", &temp);
+            // считаем количество положительных
+            if (temp > 0)
+                positive++;
+        }
+        // процент положительных чисел
+        percent = positive * 100 / count;
+
+        // запись в файл
+        fprintf(f1, "\n%.2f", percent);
+
+        printf("\nПроцент положительных чисел %.2f", percent);
+
+        fclose(f1);
+        getchar();
+        exit(0);
+    }
+}
